47b109f9ab4a6e0a591c337078f350e42bf2f2da
gaurav <gauravddja@gmail.com>
2018-02-13 12:50:33 +0530

    Luck Balance

--- src/main/java/day48/LuckBalance.java
+++ src/main/java/day48/LuckBalance.java
@@ -2,6 +2,10 @@ package day48;
 
 import template.FastInputReader;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Gaurav on 12/02/18.
  */
@@ -12,17 +16,37 @@ public class LuckBalance {
         int N = fastInputReader.readInt();
         int K = fastInputReader.readInt();
 
-        int[] L = new int[N];
-        int[] T = new int[N];
+        long totalLuck = 0;
+        List<Integer> integers
+                = new ArrayList<>();
+
+
         for (int i = 0; i < N; i++) {
-            L[i] = fastInputReader.readInt();
-            T[i] = fastInputReader.readInt();
+            int luck = fastInputReader.readInt();
+            int importance = fastInputReader.readInt();
+
+            totalLuck += luck;
+
+            if (importance == 1) {
+                integers.add(luck);
+            }
         }
+        Collections.sort(integers);
 
-        solution(L,T);
+        //As we have to loose maximum of K(so calculate the maximum one) competition so better to be loose with mininmum luck one . so sot
+        //the minimum luck and win them. and we can lose the less important competition without taking care of luck
+
+        int size = integers.size() - K;
+        int luck = 0;
+        for (int i = 0; i < size; i++) {
+            luck += integers.get(i);
+        }
+            //multiplying with 2 as we have already calculated the luck in total luck
+            // so if we are losing the competition the instead of gaining X point we are losing the X point so total - 2 * X
+        System.out.println(totalLuck - 2 * luck);
+
+
+
     }
 
-    private static void solution(int[] l, int[] t) {
-
-    }
 }
