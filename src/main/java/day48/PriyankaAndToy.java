921f708d71b75188d35479d65942d5d0ff62c412
gaurav <gauravddja@gmail.com>
2018-02-14 10:32:06 +0530

    Priyanka and toys

---
+++ src/main/java/day48/PriyankaAndToy.java
@@ -0,0 +1,42 @@
+package day48;
+
+import template.FastInputReader;
+
+import java.util.Arrays;
+
+/**
+ * Created by Gaurav on 14/02/18.
+ */
+public class PriyankaAndToy {
+
+    public static void main(String[] args) {
+        FastInputReader fastInputReader = new FastInputReader();
+        int T = fastInputReader.readInt();
+        int[] toys = new int[T];
+        for (int i = 0; i < T; i++) {
+            toys[i] = fastInputReader.readInt();
+        }
+
+        System.out.println(solution(toys));
+    }
+
+    private static int solution(int[] toys) {
+
+
+        Arrays.sort(toys);
+        int start = toys[0];
+        int total = 1;
+        for (int i = 1; i < toys.length; i++) {
+
+            if (toys[i] <= start + 4) {
+                //do nothing
+            } else {
+                start = toys[i];
+                total++;
+            }
+
+        }
+        return total;
+
+    }
+}
