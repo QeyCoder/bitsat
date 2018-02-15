560ffe4dde92e2ee11699e5fe53caf476e2c6980
gaurav <gauravddja@gmail.com>
2018-02-15 21:20:58 +0530

    twitter

---
+++ src/main/java/day50/ClosingTheTweet.java
@@ -0,0 +1,42 @@
+package day50;
+
+import template.FastInputReader;
+
+/**
+ * Created by Gaurav on 15/02/18.
+ */
+public class ClosingTheTweet {
+
+    public static void main(String[] args) {
+        FastInputReader fastInputReader
+                = new FastInputReader();
+        int N = fastInputReader.readInt();
+        int k = fastInputReader.readInt();
+        int[] tweets = new int[N];
+
+        int openTweet = 0;
+        for (int i = 0; i < k; i++) {
+            String input = fastInputReader.readString();
+            if (input.equals("CLICK")) {
+                int index = fastInputReader.readInt() - 1;
+                if (tweets[index] == 0) {
+                    tweets[index] = 1;
+                    openTweet++;
+                } else {
+                    tweets[index] = 0;
+                    openTweet--;
+                }
+
+            } else {
+                for (int j = 0; j < N; j++) {
+                    tweets[j] = 0;
+                    openTweet = 0;
+                }
+            }
+
+            System.out.println(openTweet);
+
+
+        }
+    }
+}
