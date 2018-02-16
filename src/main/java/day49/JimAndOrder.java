43d2700f6172747e3e0faf778d44412d68db0597
gaurav <gauravddja@gmail.com>
2018-02-16 20:37:53 +0530

    JIm and order

--- src/main/java/day49/JimAndOrder.java
+++ src/main/java/day49/JimAndOrder.java
@@ -2,6 +2,9 @@ package day49;
 
 import template.FastInputReader;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Created by Gaurav on 14/02/18.
  */
@@ -10,16 +13,43 @@ public class JimAndOrder {
     public static void main(String[] args) {
         FastInputReader fastInputReader = new FastInputReader();
         int n = fastInputReader.readInt();
-        int[] t = new int[n];
-        int[] d = new int[n];
+        Pair[] t1 = new Pair[n];
+
         for (int i = 0; i < n; i++) {
-            t[i] = fastInputReader.readInt();
-            d[i] = fastInputReader.readInt();
+            int t = fastInputReader.readInt();
+            int val = t + fastInputReader.readInt();
+
+            Pair pair = new Pair(i + 1, val);
+            t1[i] = pair;
         }
 
-        solution(t,d);
+        solution(t1);
     }
 
+    private static void solution(Pair[] t1) {
+
+        Arrays.sort(t1, new Comparator<Pair>() {
+            @Override
+            public int compare(Pair o1, Pair o2) {
+                return o1.val - o2.val;
+            }
+        });
+
+
+        for (int i = 0; i < t1.length; i++) {
+            System.out.print(t1[i].index + " ");
+        }
+    }
+
+    static class Pair {
+        int val;
+        int index;
+
+        public Pair(int i, int val) {
+            this.index = i;
+            this.val = val;
+        }
+    }
     private static void solution(int[] t, int[] d) {
 
     }
