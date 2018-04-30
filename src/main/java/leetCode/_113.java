ab72687ef2b495fe2ad12ae6c07e1918001b7b47
gaurav <gauravddja@gmail.com>
2018-04-30 16:10:20 +0530

    path sum

--- src/main/java/leetCode/_113.java
+++ src/main/java/leetCode/_113.java
@@ -1,6 +1,7 @@
 package leetCode;
 
-import template.FastInputReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Gaurav on 30/04/18.
@@ -10,25 +11,54 @@ import template.FastInputReader;
 public class _113 {
 
 
-    public static void main(String[] args) {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> finalPath = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        binaryTreePaths(root, path, finalPath, sum);
+        return finalPath;
+    }
 
-     /*_113 ref =  new _113();
-        ref.solution();
-        */
-        FastInputReader fastInputReader = new FastInputReader();
-        int T = fastInputReader.readInt();
-        StringBuilder stringBuilder = new StringBuilder();
-        while (T-- > 0) {
+    public void binaryTreePaths(TreeNode root, List<Integer> path, List<List<Integer>> finalPath, int sum) {
 
-            int N = fastInputReader.readInt();
-            stringBuilder.append(solution()).append("\n");
+        if (root == null)
+            return;
+        sum -= root.val;
+        path.add(root.val);
+        if (root.left == null && root.right == null) {
+
+
+            if (0 == sum)
+                finalPath.add(new ArrayList<Integer>(path));
+
+        } else {
+            binaryTreePaths(root.left, path, finalPath, sum);
+            binaryTreePaths(root.right, path, finalPath, sum);
         }
-        System.out.println(stringBuilder);
+        path.remove(path.size() - 1);
+
+        }
+
+    public static void main(String[] args) {
+        _113
+                obj = new _113();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(5);
+
+        List<List<Integer>> path = obj.pathSum(root, 8);
+        System.out.printf("s");
     }
 
-    private static int solution() {
-        return 0;
-    }
 
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+    }
+    }
 
 }
