fa4c759daac2ab90bfd39d8517571608d4ddf556
Gaurav Dudeja <Gaurav.Dudeja@go-mmt.com>
2020-05-20 01:06:02 +0530

    881

---
+++ src/main/java/roundone/Solution881.java
@@ -0,0 +1,28 @@
+package roundone;
+
+import java.util.Arrays;
+
+public class Solution881 {
+        public int numRescueBoats(int[] people, int limit) {
+            Arrays.sort(people);
+            int i=0;
+            int j= people.length-1;
+            int boat=0;
+
+            while(i<=j){
+                int diff= limit-people[j];
+                j--;
+                int max=0;
+                while(i<people.length-1&&diff>=people[i]){
+                    if(max==1){
+                        break;
+                    }
+                    diff=diff-people[i];
+                    max++;
+                    i++;
+                }
+                boat++;
+            }
+            return boat;
+        }
+}
