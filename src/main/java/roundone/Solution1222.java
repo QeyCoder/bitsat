0b5d297d8d3101b882583643a1d26b43994cfc3e
Gaurav Dudeja <Gaurav.Dudeja@go-mmt.com>
2020-05-19 23:58:18 +0530

    1222

---
+++ src/main/java/roundone/Solution1222.java
@@ -0,0 +1,87 @@
+package roundone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution1222 {
+        public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+            List<List<Integer>> ans = new ArrayList<>();
+            for (int i = 0; i < queens.length; i++) {
+                boolean canAttack = getSolution(queens, king, i);
+                if (canAttack) {
+                    List<Integer> solvedList = new ArrayList<>();
+                    solvedList.add(queens[i][0]);
+                    solvedList.add(queens[i][1]);
+                    ans.add(solvedList);
+                }
+            }
+            return ans;
+        }
+
+        boolean getSolution(int[][] queens, int[] king, int currentQueen) {
+            int[][] board = new int[8][8];
+            ;
+            for (int i = 0; i < queens.length; i++) {
+                if (i != currentQueen) {
+                    board[queens[i][0]][queens[i][1]] = 1;
+                }
+            }
+
+            return solve(board, king[0], king[1], queens[currentQueen][0], queens[currentQueen][1]);
+
+
+        }
+
+
+        boolean solve(int[][] board, int kingX, int kingY, int x, int y) {
+            if (x < 0 || x == 8 || y < 0 || y == 8) {
+                return false;
+            }
+            if (kingX == x && kingY == y) {
+                return true;
+            }
+            if (board[x][y] != 0) {
+                return false;
+            }
+            board[x][y] = 1;
+            if (kingX == x && kingY != y) {
+                boolean ans = solve(board, kingX, kingY, x, y + 1);
+                if (revertAns(board, x, y, ans)) return true;
+                ans = solve(board, kingX, kingY, x, y - 1);
+                if (revertAns(board, x, y, ans)) return true;
+            } else if (kingY == y && kingX != x) {
+                boolean ans = solve(board, kingX, kingY, x + 1, y);
+                if (revertAns(board, x, y, ans)) return true;
+                ans = solve(board, kingX, kingY, x - 1, y);
+                if (revertAns(board, x, y, ans)) return true;
+            } else if (kingX + kingY == x + y) {
+                boolean ans = solve(board, kingX, kingY, x - 1, y + 1);
+                if (revertAns(board, x, y, ans)) return true;
+                ans = solve(board, kingX, kingY, x + 1, y - 1);
+                if (revertAns(board, x, y, ans)) return true;
+            } else if (kingX - kingY == x - y) {
+                boolean ans = solve(board, kingX, kingY, x - 1, y - 1);
+                if (revertAns(board, x, y, ans)) return true;
+                ans = solve(board, kingX, kingY, x + 1, y + 1);
+                if (revertAns(board, x, y, ans)) return true;
+            }
+            board[x][y] = 0;
+            return false;
+
+        }
+
+
+        private boolean revertAns(int[][] board, int x, int y, boolean ans) {
+            if (!ans) {
+                board[x][y] = 0;
+            } else {
+                return true;
+            }
+            return false;
+        }
+
+    public static void main(String[] args) {
+        new Solution1222()
+                .queensAttacktheKing(new int[][]{{0, 1}, {1, 0}, {4, 0}, {0, 4}, {3, 3}, {2, 4}}, new int[]{0, 0});
+    }
+}
