0ed8efc7599f9a3810e245f36652a54cd4745b9a
Gaurav Dudeja <gauravddja@gmail.com>
2020-05-24 08:29:35 +0530

    5417

---
+++ src/main/java/roundone/Solution5417.java
@@ -0,0 +1,49 @@
+package roundone;
+
+public class Solution5417 {
+    class Solution {
+        public int maxVowels(String s, int k) {
+            if(s.length()==0){
+                return 0;
+            }
+            if(k>s.length()){
+                k=s.length();
+            }
+
+            int i = 0;
+            int j = 0;
+            int max = 0;
+            int vowel = 0;
+            while (j - i != k && j < s.length()) {
+
+                char ip = s.charAt(j);
+                if (ip == 'a' || ip == 'e' || ip == 'i' || ip == 'o' || ip == 'u') {
+                    vowel++;
+                }
+                j++;
+                if (j - i == k) {
+                    if (vowel > max) {
+                        max = vowel;
+                    }
+                }
+                while (j - i == k) {
+                    ip = s.charAt(i);
+                    if (ip == 'a' || ip == 'e' || ip == 'i' || ip == 'o' || ip == 'u') {
+                        vowel--;
+                    }
+                    i++;
+
+
+                }
+            }
+            return max;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution
+                = new Solution();
+        System.out.println(solution.maxVowels("abciiidef", 13));
+    }
+}
