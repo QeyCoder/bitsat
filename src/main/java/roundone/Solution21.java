2d53e88ed608f0890b00bdaa3eced34d65ea1b0d
Gaurav Dudeja <gauravddja@gmail.com>
2020-06-03 22:46:55 +0530

    21 solution2

--- src/main/java/roundone/Solution21.java
+++ src/main/java/roundone/Solution21.java
@@ -54,6 +54,23 @@ public class Solution21
             return tmp;
 
         }
+        public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+
+            if(l1==null){
+                return l2;
+            }
+            if(l2==null){
+                return l1;
+            }
+            if(l1.val< l2.val){
+                l1.next = mergeTwoLists1(l1.next,l2);
+                return l1;
+            }else{
+                l2.next = mergeTwoLists1(l1,l2.next);
+                return l2;
+            }
+
+        }
     }
     public static void main(String args[]) {
         Solution solution = new Solution();
