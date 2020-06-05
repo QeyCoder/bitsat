7fe2e89aee17b4d782d6187f41a9c8b13e704aa1
Gaurav Dudeja <gauravddja@gmail.com>
2020-06-05 10:51:01 +0530

    1025

---
+++ src/main/java/roundone/Solution1025.java
@@ -0,0 +1,16 @@
+package roundone;
+
+public class Solution1025 {
+
+    static class Solution {
+        public boolean divisorGame(int N) {
+
+
+            return (N&1)==0;
+        }
+    }
+
+    public static void main(String args[]) {
+        Solution solution = new Solution();
+    }
+}
