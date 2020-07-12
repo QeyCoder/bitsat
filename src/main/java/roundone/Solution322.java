fec45466c157a5e0d56bf5636f97c9cb04d88680
Gaurav Dudeja <gauravddja@gmail.com>
2020-07-12 13:51:03 +0530

    LC Solution 322

---
+++ src/main/java/roundone/Solution322.java
@@ -0,0 +1,69 @@
+package roundone;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution322 {
+
+    static class Solution {
+        public int coinChange(int[] coins, int amount) {
+            int[] dp = new int[amount+1];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] =0;
+            for (int coin : coins) {
+                for (int i = coin; i <= amount; i++) {
+                    if(dp[i-coin]!= Integer.MAX_VALUE){
+                        dp[i] =  Math.min(dp[i],dp[i-coin]+1);
+                    }
+                }
+            }
+            return dp[amount]==Integer.MAX_VALUE?-1: dp[amount];
+
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+    Solution solution =  new Solution();
+        System.out.println( solution.coinChange(new int[]{1,5},2));
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
+
+
